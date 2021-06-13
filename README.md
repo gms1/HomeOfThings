ca2cf1571604989f3bec0e84a612672db5202bd6
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2021-06-13 12:47:28 +0200

    version bump and codecov

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Build Status](https://api.travis-ci.com/gms1/HomeOfThings.svg?branch=master)](https://travis-ci.com/gms1/HomeOfThings)
-[![Coverage Status](https://coveralls.io/repos/github/gms1/HomeOfThings/badge.svg?branch=master&service=github)](https://coveralls.io/github/gms1/HomeOfThings?branch=master)
+[![Coverage Status](https://codecov.io/gh/gms1/HomeOfThings/branch/master/graph/badge.svg)](https://codecov.io/gh/gms1/HomeOfThings)
 [![DeepScan grade](https://deepscan.io/api/teams/439/projects/987/branches/1954/badge/grade.svg)](https://deepscan.io/dashboard#view=project&tid=439&pid=987&bid=1954)
 [![Dependency Status](https://david-dm.org/gms1/HomeOfThings.svg)](https://david-dm.org/gms1/HomeOfThings)
 [![Known Vulnerabilities](https://snyk.io/test/github/gms1/HomeOfThings/badge.svg)](https://snyk.io/test/github/gms1/HomeOfThings)
@@ -7,7 +7,7 @@
 [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 # HomeOfThings
 
-## packages for external and internal use
+## packages also intended for external use
 
 - [@homeofthings/nestjs-logger](projects/node/libs/nestjs-logger/README.md)
 a logging module for [NestJS](https://docs.nestjs.com/) based on [winston](https://www.npmjs.com/package/winston)
--- build/version-bump.ts
+++ build/version-bump.ts
@@ -10,17 +10,19 @@ const workdir = process.cwd();
 const workspaceDir = path.relative(workdir, path.resolve(__dirname, '..'));
 
 const workspaceJsonPath = path.join(workspaceDir, 'workspace.json');
-const packageJsonPath = path.join(workspaceDir, 'package.json');
+const rootPackageJsonPath = path.join(workspaceDir, 'package.json');
 const versionRegex = /^(\d+)\.(\d+)\.(\d+)((-rc\.)(\d+))?$/;
+
+const DEFAULT_PACKAGE_PREFIX = 'hot-';
 
 // -----------------------------------------------------------------------------------------
 const program = new Command();
 program
   .version('1.0')
-  .command(`bump [<new-version>|keep]`, { isDefault: true })
+  .command(`bump [<new-version>|keep|increment] [package]`, { isDefault: true })
   .description('bump version for all projects')
-  .action(async (version?: string) => {
-    return versionBump(version)
+  .action(async (version?: string, packageName?: string) => {
+    return versionBump(version, packageName)
       .catch((err) => {
         die(`failed: ${err}`);
       })
@@ -31,21 +33,26 @@ program
 program.parse(process.argv);
 
 // -----------------------------------------------------------------------------------------
-async function versionBump(version?: string) {
+async function versionBump(version?: string, packageName?: string) {
   const workspace = await readJson(workspaceJsonPath);
   if (!workspace.projects) {
     warn(`no projects are currently defined`);
     return;
   }
+  const packageJsonPath = packageName ? path.join(workspaceDir, workspace.projects[packageName].root, 'package.json') : rootPackageJsonPath;
   const packageJson = await readJson(packageJsonPath);
   const newVersion = getNewVersion(packageJson, version);
-  await bumpPackageVersion(packageJsonPath, newVersion);
-  await Promise.all(
-    Object.keys(workspace.projects).map((projectName) => {
-      const projectPackageJson = path.join(workspaceDir, workspace.projects[projectName].root, 'package.json');
-      return bumpPackageVersion(projectPackageJson, newVersion);
-    }),
-  );
+  await bumpPackageVersion('workspace', packageJsonPath, newVersion);
+  if (!packageName) {
+    await Promise.all(
+      Object.keys(workspace.projects)
+        .filter((projectName) => projectName.substring(0, DEFAULT_PACKAGE_PREFIX.length) === DEFAULT_PACKAGE_PREFIX)
+        .map((projectName) => {
+          const projectPackageJson = path.join(workspaceDir, workspace.projects[projectName].root, 'package.json');
+          return bumpPackageVersion(projectName, projectPackageJson, newVersion);
+        }),
+    );
+  }
 }
 
 // -----------------------------------------------------------------------------------------
@@ -54,22 +61,22 @@ function getNewVersion(packageJson: any, version?: string): string {
   if (!currentVersionParts) {
     throw new Error(`current version '${packageJson.version}' is invalid`);
   }
-  if (version && version !== 'keep') {
+  if (version && version !== 'keep' && version !== 'increment') {
     if (!version.match(versionRegex)) {
       throw new Error(`new version '${version}' is invalid`);
     }
     return version;
   }
-  const inc = version === 'keep' ? 0 : 1;
+  const increment = version === 'keep' ? 0 : 1;
   if (currentVersionParts[4]) {
-    return `${currentVersionParts[1]}.${currentVersionParts[2]}.${currentVersionParts[3]}${currentVersionParts[5]}` + (inc + parseInt(currentVersionParts[6], 10)).toString();
+    return `${currentVersionParts[1]}.${currentVersionParts[2]}.${currentVersionParts[3]}${currentVersionParts[5]}` + (increment + parseInt(currentVersionParts[6], 10)).toString();
   } else {
-    return `${currentVersionParts[1]}.${currentVersionParts[2]}.` + (inc + parseInt(currentVersionParts[3], 10)).toString();
+    return `${currentVersionParts[1]}.${currentVersionParts[2]}.` + (increment + parseInt(currentVersionParts[3], 10)).toString();
   }
 }
 
 // -----------------------------------------------------------------------------------------
-async function bumpPackageVersion(filePath: string, newVersion: string): Promise<void> {
+async function bumpPackageVersion(projectName: string, filePath: string, newVersion: string): Promise<void> {
   const stat = await statFilePromised(filePath);
   if (!stat) {
     return;
@@ -80,8 +87,8 @@ async function bumpPackageVersion(filePath: string, newVersion: string): Promise
   if (oldVersion !== newVersion) {
     packageJson.version = newVersion;
     await writeJson(filePath, packageJson);
-    log(`  ${filePath}: changed ${oldVersion} => ${newVersion}`);
+    log(`  ${projectName}: ${filePath}: changed ${oldVersion} => ${newVersion}`);
   } else {
-    log(`  ${filePath}: ${oldVersion}`);
+    log(`  ${projectName}: ${filePath}: ${oldVersion}`);
   }
 }
