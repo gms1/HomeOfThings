0f437b97dfa877056856dc13994dc6049065779d
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2021-08-18 05:53:16 +0200

    nx migrate latest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
+const { getJestProjects } = require('@nrwl/jest');
+
 const { defaults } = require('jest-config');
-module.exports = {
-  projects: [
-    '<rootDir>/projects/node/libs/nestjs-config',
-    '<rootDir>/projects/node/apps/hot-gateway',
-    '<rootDir>/projects/node/libs/nestjs-logger',
-    '<rootDir>/projects/node/libs/hot-express',
-  ],
-};
+module.exports = { projects: getJestProjects() };
--- projects/node/apps/hot-gateway/jest.config.js
+++ projects/node/apps/hot-gateway/jest.config.js
@@ -11,4 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../../../coverage/projects/node/apps/hot-gateway',
+  testEnvironment: 'node',
 };
