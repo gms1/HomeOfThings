cb5500dce1f1cdfccc6a1dcc9a42f5afb8f93468
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2021-09-01 09:25:00 +0200

    build: prepare publishing

---
+++ build/prepare-publish.ts
@@ -0,0 +1,83 @@
+#!/usr/bin/env ts-node-script
+import * as path from 'path';
+import * as process from 'process';
+import { Command } from 'commander';
+import { die, log, readJson, setApplication, warn, writeJson } from './ts/common';
+
+setApplication(__filename);
+const workdir = process.cwd();
+const workspaceDir = path.relative(workdir, path.resolve(__dirname, '..'));
+
+const workspaceJsonPath = path.join(workspaceDir, 'workspace.json');
+
+// -----------------------------------------------------------------------------------------
+const program = new Command();
+program
+  .version('1.0')
+  .command(`prepare-publish`, { isDefault: true })
+  .description('prepare all projects for publishing')
+  .action(async () => {
+    return preparePublishing()
+      .catch((err) => {
+        die(`failed: ${err}`);
+      })
+      .then(() => {
+        log(`succeeded`);
+      });
+  });
+program.parse(process.argv);
+
+async function preparePublishing() {
+  try {
+    const workspace = await readJson(workspaceJsonPath);
+    if (!workspace.projects) {
+      warn(`no projects are currently defined`);
+      return;
+    }
+    await Promise.all(
+      Object.keys(workspace.projects)
+        .filter((projectName) => workspace.projects[projectName].projectType === 'library')
+        .sort((a, b) => a.localeCompare(b))
+        .map((projectName) => prepareProject(projectName, path.join(workspace.projects[projectName].targets?.build?.options?.outputPath, 'package.json'))),
+    );
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
+
+async function prepareProject(projectName: string, filePath?: string): Promise<void> {
+  try {
+    const packageJson = await readJson(filePath);
+
+    // special dependencies as peer dependencies
+    if (packageJson.dependencies) {
+      const peerDependencies = Object.keys(packageJson.dependencies).filter((dep) => dep.startsWith('@nestjs/') || dep.startsWith('@angular/') || dep === 'rxjs');
+      if (peerDependencies.length) {
+        packageJson.peerDependencies = packageJson.peerDependencies || {};
+        peerDependencies.forEach((dep) => {
+          let depVersion = packageJson.dependencies[dep];
+          if (depVersion.startsWith('~')) {
+            depVersion = '^' + depVersion.substring(1);
+          }
+          packageJson.peerDependencies[dep] = depVersion;
+          delete packageJson.dependencies[dep];
+        });
+        if (!Object.keys(packageJson.dependencies).length) {
+          delete packageJson.dependencies;
+        }
+      }
+    }
+
+    // enable publishing
+    if (packageJson.scripts) {
+      delete packageJson.scripts['prepublishOnly'];
+      if (!Object.keys(packageJson.scripts).length) {
+        delete packageJson.scripts;
+      }
+    }
+    await writeJson(filePath, packageJson);
+    log(`${packageJson.name} ${packageJson.version} done`);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
--- build/version-bump.ts
+++ build/version-bump.ts
@@ -3,7 +3,6 @@ import * as path from 'path';
 import * as process from 'process';
 import { Command } from 'commander';
 import { die, log, readJson, setApplication, warn, writeJson } from './ts/common';
-import { statFilePromised } from './ts/utils/fs';
 
 setApplication(__filename);
 const workdir = process.cwd();
@@ -19,7 +18,7 @@ const DEFAULT_PACKAGE_PREFIX = 'hot-';
 const program = new Command();
 program
   .version('1.0')
-  .command(`bump [<new-version>|keep|increment] [package]`, { isDefault: true })
+  .command(`bump [<new-version>|increment|keep] [package]`, { isDefault: true })
   .description('bump version for all projects')
   .action(async (version?: string, packageName?: string) => {
     return versionBump(version, packageName)
@@ -34,24 +33,40 @@ program.parse(process.argv);
 
 // -----------------------------------------------------------------------------------------
 async function versionBump(version?: string, packageName?: string) {
-  const workspace = await readJson(workspaceJsonPath);
-  if (!workspace.projects) {
-    warn(`no projects are currently defined`);
-    return;
-  }
-  const packageJsonPath = packageName ? path.join(workspaceDir, workspace.projects[packageName].root, 'package.json') : rootPackageJsonPath;
-  const packageJson = await readJson(packageJsonPath);
-  const newVersion = getNewVersion(packageJson, version);
-  await bumpPackageVersion('workspace', packageJsonPath, newVersion);
-  if (!packageName) {
-    await Promise.all(
-      Object.keys(workspace.projects)
-        .filter((projectName) => projectName.substring(0, DEFAULT_PACKAGE_PREFIX.length) === DEFAULT_PACKAGE_PREFIX)
-        .map((projectName) => {
-          const projectPackageJson = path.join(workspaceDir, workspace.projects[projectName].root, 'package.json');
-          return bumpPackageVersion(projectName, projectPackageJson, newVersion);
-        }),
-    );
+  try {
+    const workspace = await readJson(workspaceJsonPath);
+    if (!workspace.projects) {
+      warn(`no projects are currently defined`);
+      return;
+    }
+    if (packageName) {
+      const projectPackageJson = path.join(workspaceDir, workspace.projects[packageName].root, 'package.json');
+      await bumpPackageVersion(packageName, projectPackageJson, version);
+    } else {
+      await bumpPackageVersion('hot-workspace', rootPackageJsonPath, version);
+      await Promise.all(
+        Object.keys(workspace.projects)
+          .filter((projectName) => projectName.substring(0, DEFAULT_PACKAGE_PREFIX.length) === DEFAULT_PACKAGE_PREFIX)
+          .sort((a, b) => a.localeCompare(b))
+          .map((projectName) => {
+            const projectPackageJson = path.join(workspaceDir, workspace.projects[projectName].root, 'package.json');
+            return bumpPackageVersion(projectName, projectPackageJson, version);
+          }),
+      );
+      if (version === 'keep') {
+        await Promise.all(
+          Object.keys(workspace.projects)
+            .filter((projectName) => projectName.substring(0, DEFAULT_PACKAGE_PREFIX.length) !== DEFAULT_PACKAGE_PREFIX)
+            .sort((a, b) => a.localeCompare(b))
+            .map((projectName) => {
+              const projectPackageJson = path.join(workspaceDir, workspace.projects[projectName].root, 'package.json');
+              return bumpPackageVersion(projectName, projectPackageJson, version);
+            }),
+        );
+      }
+    }
+  } catch (err) {
+    return Promise.reject(err);
   }
 }
 
@@ -76,19 +91,21 @@ function getNewVersion(packageJson: any, version?: string): string {
 }
 
 // -----------------------------------------------------------------------------------------
-async function bumpPackageVersion(projectName: string, filePath: string, newVersion: string): Promise<void> {
-  const stat = await statFilePromised(filePath);
-  if (!stat) {
-    return;
-  }
+async function bumpPackageVersion(projectName: string, filePath: string, version: string): Promise<void> {
+  try {
+    const packageJson = await readJson(filePath);
+    const newVersion = getNewVersion(packageJson, version);
 
-  const packageJson = await readJson(filePath);
-  const oldVersion = packageJson.version;
-  if (oldVersion !== newVersion) {
-    packageJson.version = newVersion;
-    await writeJson(filePath, packageJson);
-    log(`  ${projectName}: ${filePath}: changed ${oldVersion} => ${newVersion}`);
-  } else {
-    log(`  ${projectName}: ${filePath}: ${oldVersion}`);
+    const oldVersion = packageJson.version;
+    if (oldVersion !== newVersion) {
+      packageJson.version = newVersion;
+      await writeJson(filePath, packageJson);
+      log(`  ${projectName}: ${filePath}: changed ${oldVersion} => ${newVersion}`);
+    } else {
+      log(`  ${projectName}: ${filePath}: ${oldVersion}`);
+    }
+    return;
+  } catch (err) {
+    return Promise.reject(err);
   }
 }
