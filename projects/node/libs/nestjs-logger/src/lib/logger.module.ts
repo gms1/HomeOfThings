2892eb1169b4fe80ff43a1746388bced58dc7263
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2021-09-11 10:30:05 +0200

    nestjs-logger: global module ( no need to import either LoggerModule or LoggerModule.forChild() )

--- projects/node/libs/nestjs-logger/src/lib/logger.module.ts
+++ projects/node/libs/nestjs-logger/src/lib/logger.module.ts
@@ -3,11 +3,12 @@ import { LoggerModuleOptions, LOGGER_MODULE_OPTIONS_TOKEN } from './model';
 import { LoggerService } from './logger.service';
 import { createDynamicRootModule } from '@homeofthings/nestjs-utils';
 
-@Module({
+@Module({})
+export class LoggerModule extends createDynamicRootModule<LoggerModule, LoggerModuleOptions>(LOGGER_MODULE_OPTIONS_TOKEN, {
+  global: true,
   providers: [LoggerService],
   exports: [LoggerService],
-})
-export class LoggerModule extends createDynamicRootModule<LoggerModule, LoggerModuleOptions>(LOGGER_MODULE_OPTIONS_TOKEN) {
+}) {
   static createLoggerService(options: LoggerModuleOptions): LoggerService {
     return new LoggerService(options);
   }
