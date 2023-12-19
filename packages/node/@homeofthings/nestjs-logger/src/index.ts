7e02c97fab804c25d4df711c0f0809b9ba0e7566
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2023-12-19 11:35:00 +0100

    feat: additional exports

--- packages/node/@homeofthings/nestjs-logger/src/index.ts
+++ packages/node/@homeofthings/nestjs-logger/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/model';
 export * from './lib/nest-logger.service';
 export * from './lib/logger.service';
 export * from './lib/logger.module';
+export * from './lib/winston/winston-logger';
+export * from './lib/winston/winston-format';
