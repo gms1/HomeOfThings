65a9585e0eee66221fa042b37f986d1cd7764d36
Guenter Sandner <guenter.sandner@smartstream-stp.com>
2024-01-02 16:20:35 +0100

    style: renamed BaseConfigService to NodeUtilsConfigService

--- packages/node/@homeofthings/nestjs-config/src/lib/config.service.ts
+++ packages/node/@homeofthings/nestjs-config/src/lib/config.service.ts
@@ -1,4 +1,4 @@
-import { ConfigService as BaseConfigService } from '@homeofthings/node-utils';
+import { ConfigService as NodeUtilsConfigService } from '@homeofthings/node-utils';
 import { Inject, Injectable } from '@nestjs/common';
 
 import { CONFIG_MODULE_OPTIONS_TOKEN, ConfigModuleOptions } from './model';
@@ -7,7 +7,7 @@ process.env.SUPPRESS_NO_CONFIG_WARNING = '1';
 
 /** ConfigService to read configured values. */
 @Injectable()
-export class ConfigService extends BaseConfigService {
+export class ConfigService extends NodeUtilsConfigService {
   constructor(@Inject(CONFIG_MODULE_OPTIONS_TOKEN) _opts: ConfigModuleOptions) {
     super(_opts);
   }
